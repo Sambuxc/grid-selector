0a4c1e23e90664f8da575064633316a91855c304
Sambuxc <samwbru@gmail.com>
2023-11-18 10:22:40 +0000

    mouse over grid cells updates cell component state isSelected and adds the corresponding classes

--- src/components/GridCell.js
+++ src/components/GridCell.js
@@ -1,7 +1,23 @@
+const baseClass = "grid__cell "
+const selected = "selected"
+
 const GridCell = () => ({
   tag: "div",
-  class: "grid__cell",
+  class: (elem, state) => (state.isSelected ? baseClass + selected : baseClass),
   text: (elem, state) => elem.key,
+
+  state: {
+    isSelected: false,
+    classList: baseClass,
+  },
+
+  on: {
+    mouseover: (event, elem, state) => {
+      state.update({
+        isSelected: !state.isSelected,
+      })
+    },
+  },
 })
 
 export default GridCell
